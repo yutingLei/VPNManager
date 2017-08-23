b6233a6abfdfd366840a0a033bbd77fe2d6a6d93
yutingLei <lyt_930617@163.com>
2017-08-23 10:30:48 +0800

    Initial Commit

---
+++ VPNManager/VPNManager.h
@@ -0,0 +1,19 @@
+//
+//  VPNManager.h
+//  VPNManager
+//
+//  Created by admin on 8/23/17.
+//  Copyright © 2017 Favorite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPNManager.
+FOUNDATION_EXPORT double VPNManagerVersionNumber;
+
+//! Project version string for VPNManager.
+FOUNDATION_EXPORT const unsigned char VPNManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPNManager/PublicHeader.h>
+
+
